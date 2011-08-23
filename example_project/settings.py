6ee98e0c37df2d8a7c4cc689b961c1789dbeb579
Rafał Selewońko <rafal@selewonko.com>
2011-08-23 23:48:23 +0200

    Tried to make debug_toolbar work

--- example_project/settings.py
+++ example_project/settings.py
@@ -635,8 +635,25 @@ if DEBUG:
         pass
     else:
         # Insert DDT after the common middleware
-        common_index = MIDDLEWARE_CLASSES.index('django.middleware.common.CommonMiddleware')
-        MIDDLEWARE_CLASSES.insert(common_index+1, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+#        common_index = MIDDLEWARE_CLASSES.index('django.middleware.common.CommonMiddleware')
+        MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
         if '127.0.0.1' not in INTERNAL_IPS: 
             INTERNAL_IPS = INTERNAL_IPS + ('127.0.0.1',)
         INSTALLED_APPS.append('debug_toolbar')
+        
+        DEBUG_TOOLBAR_PANELS = (
+            'debug_toolbar.panels.version.VersionDebugPanel',
+            'debug_toolbar.panels.timer.TimerDebugPanel',
+            'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+            'debug_toolbar.panels.headers.HeaderDebugPanel',
+            'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+            'debug_toolbar.panels.template.TemplateDebugPanel',
+            'debug_toolbar.panels.sql.SQLDebugPanel',
+            'debug_toolbar.panels.signals.SignalDebugPanel',
+            'debug_toolbar.panels.logger.LoggingPanel',
+        )
+        
+        DEBUG_TOOLBAR_CONFIG = {
+            'INTERCEPT_REDIRECTS': False,
+            'HIDE_DJANGO_SQL': False,
+        }
